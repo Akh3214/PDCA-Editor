4804e93fa3bd3f293aac5a69429610d7ca0e0092
Akira Hasegawa <ha_ra1515@hotmail.co.jp>
2016-07-08 08:17:03 +0900

    testing

--- pdca-editor.c
+++ pdca-editor.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
-	printf("%d\n", dateplusdays(20160908, 5));
+	int n;
+	time_t timer;
+	time(&timer);
+	struct tm *tm_now;
+	tm_now = localtime(&timer);
+	
+	const int date = (tm_now->tm_year + 1900) * 10000 + (tm_now->tm_mon + 1)*100 + tm_now->tm_mday;
+	for(;;) {
+	scanf("%d", &n);
+	printf("%d\n", dateplusdays(date, n));
+	}
 	return EXIT_SUCCESS;
 }
